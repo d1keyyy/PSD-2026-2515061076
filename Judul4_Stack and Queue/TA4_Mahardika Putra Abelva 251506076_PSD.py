2d3a295ce75f45ab5a2bc9e1fcc02a585d93a662
d1keyyy <159967606+d1keyyy@users.noreply.github.com>
2026-05-19 22:33:33 +0700

    typo

--- Judul4_Stack and Queue/TA4_Mahardika Putra Abelva 251506076_PSD.py
+++ Judul4_Stack and Queue/TA4_Mahardika Putra Abelva 251506076_PSD.py
@@ -94,7 +94,7 @@ def main():
     pilih = 0
 
     while pilih != 8:
-        print("\n=== Manajemen Antrian Pengunjung Perpustakaan ===")
+        print("\Sistem Antrian Kasir Cafe")
         print("1. Enqueue")
         print("2. Dequeue")
         print("3. Peek")
