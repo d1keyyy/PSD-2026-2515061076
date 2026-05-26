4ff02f7d20fba65c88ae4a02bbf5ac770fd92f00
d1keyyy <159967606+d1keyyy@users.noreply.github.com>
2026-05-26 20:56:35 +0700

    Refactor contact management system with updates

--- Judul5_Binary Search Tree/TA5_Mahardika Putra Abelva.py
+++ Judul5_Binary Search Tree/TA5_Mahardika Putra Abelva.py
@@ -102,52 +102,52 @@ def main():
 
     while pilih != 8:
         print("\nSistem Save Kontak Fineshyt")
-        print("1. Tambah Kontak")
-        print("2. Cari Kontak")
-        print("3. Tampilkan Inorder")
-        print("4. Tampilkan Preorder")
-        print("5. Tampilkan Postorder")
-        print("6. Jumlah Kontak")
-        print("7. Kontak Awal & Akhir")
+        print("1. Tambah Kontak Fineshyt")
+        print("2. Cari Kontak Fineshyt")
+        print("3. Urutkan Inorder")
+        print("4. Urutkan Preorder")
+        print("5. Urutkan Postorder")
+        print("6. Jumlah Kontak Fineshyt")
+        print("7. Kontak Fineshyt Awal & Akhir")
         print("8. Keluar")
 
         pilih = int(input("Pilih menu: "))
 
         if pilih == 1:
-            nama = input("Masukkan nama kontak : ")
-            nomor = input("Masukkan nomor telepon : ")
+            nama = input("Masukkan nama Fineshyt : ")
+            nomor = input("Masukkan nomor telepon Fineshyt : ")
 
             kontak.insert(nama, nomor)
 
-            print("Kontak berhasil ditambahkan")
+            print("Fineshyt berhasil ditambahkan")
 
         elif pilih == 2:
-            nama = input("Masukkan nama yang dicari : ")
+            nama = input("Masukkan Fineshyt yang dicari : ")
 
             hasil = kontak.search(nama)
 
             if hasil is not None:
-                print("Kontak ditemukan")
+                print("Kontak Fineshyt ditemukan")
                 print("Nama  :", hasil.key)
                 print("Nomor :", hasil.nomor)
 
             else:
-                print("Kontak tidak ditemukan")
+                print("Kontak Fineshyt tidak ditemukan")
 
         elif pilih == 3:
-            print("\nData kontak inorder:")
+            print("\nData kontak Fineshyt inorder:")
             kontak.inorder(kontak.root)
 
         elif pilih == 4:
-            print("\nData kontak preorder:")
+            print("\nData kontak Fineshyt preorder:")
             kontak.preorder(kontak.root)
 
         elif pilih == 5:
-            print("\nData kontak postorder:")
+            print("\nData kontak Fineshyt postorder:")
             kontak.postorder(kontak.root)
 
         elif pilih == 6:
-            print("Jumlah kontak:",
+            print("Jumlah kontak Fineshyt:",
                   kontak.count_contacts(kontak.root))
 
         elif pilih == 7:
@@ -155,15 +155,15 @@ def main():
             akhir = kontak.find_last_contact(kontak.root)
 
             if awal is not None:
-                print("\nKontak pertama alfabet:")
+                print("\nKontak Fineshyt pertama alfabet:")
                 print(f"{awal.key} - {awal.nomor}")
 
             if akhir is not None:
-                print("\nKontak terakhir alfabet:")
+                print("\nKontak Fineshyt terakhir alfabet:")
                 print(f"{akhir.key} - {akhir.nomor}")
 
         elif pilih == 8:
-            print("Program selesai")
+            print("Program selesai, hope u find the love of your life soon big dawg")
 
         else:
             print("Menu tidak tersedia")
